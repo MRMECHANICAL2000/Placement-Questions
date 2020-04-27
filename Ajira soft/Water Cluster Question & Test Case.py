2656e5e6811af0d58190a059c5d1fc20a88dc2a2
MOHAMMED SAFI <43216227+MRMECHANICAL2000@users.noreply.github.com>
2020-04-28 02:27:57 +0530

    Update Water Cluster Question & Test Case.py

--- Ajira soft/Water Cluster Question & Test Case.py
+++ Ajira soft/Water Cluster Question & Test Case.py
@@ -1,3 +1,4 @@
+Water world
 Year is 3030, water is a scare resource.
 Civilizations live around glaciers in clusters, with a federal body (identified as F) in center melting glaciers and controlling the water distribution.
 Each cluster has need for water for a day and a water storage capacity.
@@ -7,8 +8,11 @@ Every time water starts flowing through pipe, the clusters drain their tanks in 
 Federal body releases water at start of day, cluster uses water at end of day.
 In a pipe link like F-C1-C2-C3-C4, when federal water body targets C3, only C3 and nodes before it (here C1,C2,C3) can fill the tank, C4 can fill it only when it’s targeted.
 Pipes are flow controlled and water flows in forward direction only.
+
 Calculate the minimum water needed to help the civilizations survive for n days.
+
 Input is multiline. First line is the number of days to survive. Second line the number of clusters followed by their definitions. Next is the number of links in the system, followed by the link definition.
+
 Read the test cases to understand the scenarios better.
 
 Cluster defintion
